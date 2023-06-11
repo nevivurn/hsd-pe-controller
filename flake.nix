b87d1f1c7fc40bd7246ede9305cd9d4775485340
Yongun Seong <nevivurn@nevi.dev>
2023-06-12 07:08:21 +0900

    add flake docker

--- flake.nix
+++ flake.nix
@@ -1,33 +1,56 @@
 {
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pydeps = pkgs.python310.withPackages
+          (ps: with ps; [
+            amaranth
+            numpy
+            pytorch
+            pytorch-lightning
+            (safetensors.overrideAttrs (prev: rec {
+              version = "0.3.1";
+              src = pkgs.fetchFromGitHub {
+                inherit (prev.src) owner repo;
+                rev = "v${version}";
+                hash = "sha256-RoIBD+zBKVzXE8OpI8GR371YPxceR4P8B9T1/AHc9vA=";
+              };
+              patches = [ ];
+              cargoDeps = prev.cargoDeps.overrideAttrs (_: { inherit patches; });
+            }))
+            torchmetrics
+            torchvision
+          ]);
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (python310.withPackages (ps: with ps; [
-              amaranth
-              numpy
-              pytorch
-              pytorch-lightning
-              (safetensors.overrideAttrs (prev: rec {
-                version = "0.3.1";
-                src = pkgs.fetchFromGitHub {
-                  inherit (prev.src) owner repo;
-                  rev = "v${version}";
-                  hash = "sha256-RoIBD+zBKVzXE8OpI8GR371YPxceR4P8B9T1/AHc9vA=";
-                };
-                patches = [ ];
-                cargoDeps = prev.cargoDeps.overrideAttrs (_: { inherit patches; });
-              }))
-              torchmetrics
-              torchvision
-            ]))
-            yosys
-          ];
+          buildInputs = [ pydeps pkgs.yosys ];
           packages = with pkgs; [ gtkwave ];
         };
+
+        packages.docker = pkgs.dockerTools.buildLayeredImage
+          {
+            name = "hsd-project";
+            contents = with pkgs; [
+              coreutils
+              pydeps
+              vim
+              yosys
+              (
+                let src = ./.; in
+                pkgs.runCommand "code" { } ''
+                  mkdir -p $out
+                  cp -rv ${src} $out/code
+                ''
+              )
+            ];
+
+            config = {
+              Cmd = [ "${pkgs.bash}/bin/bash" ];
+              WorkingDir = "code/";
+            };
+          };
       }
     );
 }
